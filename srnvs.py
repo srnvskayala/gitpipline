dcb12915d37dcdfa5fa01650acd09d87ed780571
srnvskayala <98043311+srnvskayala@users.noreply.github.com>
2022-06-20 08:44:19 +0530

    Update srnvs.py

--- srnvs.py
+++ srnvs.py
@@ -1,12 +1,14 @@
 # Python3 program to add two numbers
 
-number1 = input("First number: ")
-number2 = input("\nSecond number: ")
+a=45
+b=45
+c=a+b
+print(c)
 
 # Adding two numbers
 # User might also enter float numbers
-sum = float(number1) + float(number2)
+#sum = float(number1) + float(number2)
 
 # Display the sum
 # will print value in float
-print("The sum of {0} and {1} is {2}" .format(number1, number2, sum))
+#print("The sum of {0} and {1} is {2}" .format(number1, number2, sum))
